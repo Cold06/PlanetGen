a6fc4f2d3ff70dbd98c525f1b96bab7c0dca2c2e
cold <133922099+Cold06@users.noreply.github.com>
2024-02-15 19:21:45 -0300

    Initial commit

---
+++ Source/PlanetGenerator/PlanetGeneratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlanetGeneratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANETGENERATOR_API APlanetGeneratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
